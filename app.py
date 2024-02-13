9ec1329190446c8d42bcad96c187195c87aaf2e1
PoivronJaune <poivronjaune@gmail.com>
2024-02-12 20:28:56 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route for the root URL "/"
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Run the app
+    app.run(debug=True)
